1b004b68fc8613739591bd06cce556520599a738
Yono Mittlefehldt <yono@toojuice.com>
2022-09-08 22:06:48 +0200

    We actually need to have macOS 12 as a minimum

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DeckUI",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
