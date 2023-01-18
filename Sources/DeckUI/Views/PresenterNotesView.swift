34b1ea41baf0cbad3291b11967fd178adfb20303
zhbrass <zhbrass@gmail.com>
2023-01-18 14:02:39 -0500

    Adds a presenter notes view for macOS
    Refactored the deck state to an ObservableObject singleton so it can be used to update multiple windows and doesn't have to be passed around everywhere.
    
    Presenter notes for each slide can be added to the comments argument in the init method of a Slide.
    
    Arrow keystrokes to change the slide or highlighted line work in both the presenter view and the presenter notes view

---
+++ Sources/DeckUI/Views/PresenterNotesView.swift
@@ -0,0 +1,36 @@
+//
+//  PresenterView.swift
+//  Demo
+//
+//  Created by Zachary Brass on 1/17/23.
+//
+
+import SwiftUI
+public struct PresenterNotesView: View {
+    @ObservedObject private var presentationState = PresentationState.shared
+    
+    public var body: some View {
+        Text(presentationState.deck.slides()[presentationState.slideIndex].comment ?? "No notes")
+        .frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity)
+    }
+    public init() {
+    }
+        
+    
+}
+#if canImport(AppKit)
+@available(macOS 13.0, *)
+public struct PresenterNotes: Scene {
+    public var body: some Scene {
+        Window("Presenter Notes", id: "notes") {
+            PresenterNotesView()
+                .toolbar {
+                    SlideNavigationToolbarButtons()
+                }
+        }.keyboardShortcut("1")
+
+    }
+    public init() {
+    }
+}
+#endif
