e59cae19dc393edeab04d62eac68eb8e61a5f47d
Danny North <daniel_north@pretmail.net>
2023-05-02 14:06:10 -0500

    Allow overriding the entire theme

--- Sources/DeckUI/DSL/Theme/ThemeModifier.swift
+++ Sources/DeckUI/DSL/Theme/ThemeModifier.swift
@@ -9,23 +9,29 @@ import SwiftUI
 
 extension ContentItem {
     
+    public func theme(_ newTheme: Theme) -> some ContentItem {
+        ThemeWritingContentItem(content: self, modifier: { theme in
+            theme.merge(newTheme)
+        })
+    }
+    
     public func theme<Value>(_ keyPath: WritableKeyPath<Theme, Value>, _ value: Value) -> some ContentItem {
-        ThemeWritingContentItem(keyPath: keyPath, value: value, content: self)
+        ThemeWritingContentItem(content: self, modifier: { theme in
+            theme[keyPath: keyPath] = value
+        })
     }
     
 }
 
-private struct ThemeWritingContentItem<Value>: ContentItem {
+private struct ThemeWritingContentItem: ContentItem {
     let id = UUID()
     
-    let keyPath: WritableKeyPath<Theme, Value>
-    let value: Value
-    
     let content: any ContentItem
+    let modifier: (inout Theme) -> Void
     
     func buildView(theme: Theme) -> AnyView {
         var modified = theme
-        modified[keyPath: keyPath] = value
+        modifier(&modified)
         
         return content.buildView(theme: modified)
     }
